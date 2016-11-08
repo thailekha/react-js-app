5a122a65f420ed31bd0a897c6420d404b0fadc4f
HP <HP@yahoo>
2016-11-08 23:44:07 +0700

    container now passes userProfile of auth0 to children

--- src/Container.js
+++ src/Container.js
@@ -47,8 +47,8 @@ var Container = React.createClass({
       children = React.cloneElement(this.props.children, {
         //this.props.route is from the router
         //auth: this.props.route.auth, //sends auth instance to children
-        library: library
-        
+        library: library,
+        userProfile: U.isDefined(component.props.route.auth.getProfile()) ? component.props.route.auth.getProfile() : undefined
       })
     }
     return (
