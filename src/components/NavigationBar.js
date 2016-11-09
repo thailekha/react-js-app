855c9f9ed913515a469ed5dc02880054427f4814
HP <HP@yahoo>
2016-11-10 04:30:14 +0700

    refactor navigationbar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,22 +3,14 @@ import {Link} from 'react-router';
 import logger from '../utils/logger';
 
 //https://github.com/ReactTraining/react-router/blob/master/docs/API.md#link
-var NavigationItem = React.createClass({
-  /* ... options and lifecycle methods ... */
-  render: function() {
-    logger.reportRender('NavigationItem');
-    return (
-      <li id={"tab" + this.props.tabID}><Link to={this.props.navigationItem}>{this.props.navigationItem}</Link></li>
-    );
-  },
-});
-
 var NavigationBar = React.createClass({
   /* ... options and lifecycle methods ... */
   render: function() {
     logger.reportRender('NavigationBar');
     var navigationItems = ['home', 'browse', 'Find', 'profile', 'logout'].map(function(navItem, index) {
-      return <NavigationItem key={index} tabID={index}  navigationItem={navItem}/>
+      return (<li id={'tab' + index}>
+        <Link key={index} to={navItem}>{navItem}</Link>
+      </li>);
     }.bind(this));
     return (
       <div id="navigation">
