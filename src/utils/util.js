db5d53c94dd6b55c5a0a8a1ae95d1899c559871e
HP <HP@yahoo>
2016-11-11 03:26:37 +0700

    commit before implementing CRUD for library

--- src/utils/util.js
+++ src/utils/util.js
@@ -67,6 +67,28 @@ const U = {
       console.log(typeof component);
       component.setState({});
     });
+  },
+  deleteLibrary: function(id, component) {
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "http://localhost:3001/libraries/" + id,
+      "method": "DELETE",
+      "headers": {
+        "content-type": "application/json",
+        "cache-control": "no-cache"
+      }
+    }
+
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      console.log(response.statusCode);
+      if(response.statusCode === 200) {
+        component.setState({
+
+        });
+      }
+    });
   }
 }
 
