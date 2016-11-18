a4cedfaea47dc5441adf5d13656c15ff1cc7d4c8
HP <HP@yahoo>
2016-11-18 09:01:33 +0700

    add SubNavigationBar.test.js

---
+++ src/tests/SubNavigationBar.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import expect from 'expect';
+import {shallow} from 'enzyme';
+import SubNavigationBar from '../components/Browsepage/SubNavigationBar';
+
+describe('Component: SubNavigationBar', () => {
+  it('has SelectBox for user to choose mode, and Nav tag', () => {
+    const subnavigationbar = shallow(<SubNavigationBar subNavigationItems={[]}/>);
+
+    expect(subnavigationbar.find('SelectBox').length).toEqual(1);
+    expect(subnavigationbar.find('Nav').length).toEqual(1);
+  })
+});
+
